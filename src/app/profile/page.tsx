003cb26cd7c13d7a5a5fc8ffa09b098c139c64a2
karlo34 <marasovick1@gmail.com>
2025-05-30 13:30:50 +0200

    connected to the correct db

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,45 @@
-const Events = () => {
-    return(
-        <div>
-            <h1>Events</h1>
-        </div>
-    )
+"use client";
+import '../animations.css';
+
+import { useEffect, useState } from "react";
+
+import Navbar from '../navbar';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
 
-export default Events;
+export default function Home() {
+  const [users, setUser] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/users');
+        const data = await response.json();
+        console.log(data);
+        setUser(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Navbar/>
+      <ul>
+        {users.map((user) => (
+          <div>
+            <li key={user.id}>Ime:{user.name}</li>
+            <li>Mail:{user.email}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
